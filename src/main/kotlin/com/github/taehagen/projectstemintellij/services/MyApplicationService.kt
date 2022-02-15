7aad15c84caf4e3e19a3bbda44ef674a936629da
GitHub Action <action@github.com>
2022-02-15 02:12:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/taehagen/projectstemintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.taehagen.projectstemintellij.services
+
+import com.github.taehagen.projectstemintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/test/kotlin/com/github/taehagen/projectstemintellij/MyPluginTest.kt
+++ src/test/kotlin/com/github/taehagen/projectstemintellij/MyPluginTest.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.taehagen.projectstemintellij
 
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.psi.xml.XmlFile
